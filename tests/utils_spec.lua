8580778bdda0ee3ae27cde2860b6cf422d298710
LRDPRDX <paradox1859@gmail.com>
2026-06-25 03:59:57 +0700

    Add simple unit test

---
+++ tests/utils_spec.lua
@@ -0,0 +1,110 @@
+print(package.path)
+local li = require('likelihud')
+
+describe('utils', function()
+    local utils = li.utils
+
+    describe('textAtWidth', function()
+        describe('one character == 1 unit length', function()
+            local f = {
+                getWidth = function (_, text)
+                    return text:len()
+                end
+            }
+
+            it('empty text', function()
+                assert.is.equal(utils.textAtWidth('', 0 , f), '')
+                assert.is.equal(utils.textAtWidth('', 1 , f), '')
+                assert.is.equal(utils.textAtWidth('', 2 , f), '')
+                assert.is.equal(utils.textAtWidth('', 10, f), '')
+            end)
+
+            it('1-character text', function()
+                assert.is.equal(utils.textAtWidth('a', 0 , f), '')
+                assert.is.equal(utils.textAtWidth('a', 1 , f), 'a')
+                assert.is.equal(utils.textAtWidth('a', 2 , f), 'a')
+                assert.is.equal(utils.textAtWidth('a', 10, f), 'a')
+            end)
+
+            it('simple cases', function()
+                assert.is.equal(utils.textAtWidth('abcd', 0 , f), '')
+                assert.is.equal(utils.textAtWidth('abcd', 1 , f), 'a')
+                assert.is.equal(utils.textAtWidth('abcd', 2 , f), 'ab')
+                assert.is.equal(utils.textAtWidth('abcd', 3 , f), 'abc')
+                assert.is.equal(utils.textAtWidth('abcd', 10, f), 'abcd')
+            end)
+
+            it('general', function()
+                local text = 'abcdefghijklmnop'
+                for i = 0, text:len() do
+                    assert.is.equal(utils.textAtWidth(text, i, f), text:sub(1, i))
+                end
+            end)
+        end)
+
+        describe('one character == 1.5 unit length', function()
+            local f = {
+                getWidth = function (_, text)
+                    return text:len() * 1.5
+                end
+            }
+
+            it('empty text', function()
+                assert.is.equal(utils.textAtWidth('', 0 , f), '')
+                assert.is.equal(utils.textAtWidth('', 1 , f), '')
+                assert.is.equal(utils.textAtWidth('', 2 , f), '')
+                assert.is.equal(utils.textAtWidth('', 10, f), '')
+            end)
+
+            it('1-character text', function()
+                assert.is.equal(utils.textAtWidth('a', 0 , f), '')
+                assert.is.equal(utils.textAtWidth('a', 1 , f), '')
+                assert.is.equal(utils.textAtWidth('a', 2 , f), 'a')
+                assert.is.equal(utils.textAtWidth('a', 10, f), 'a')
+            end)
+
+            it('general', function()
+                assert.is.equal(utils.textAtWidth('abcd', 0, f), '')
+                assert.is.equal(utils.textAtWidth('abcd', 1, f), '')
+                assert.is.equal(utils.textAtWidth('abcd', 2, f), 'a')
+                assert.is.equal(utils.textAtWidth('abcd', 3, f), 'ab')
+                assert.is.equal(utils.textAtWidth('abcd', 4, f), 'ab')
+                assert.is.equal(utils.textAtWidth('abcd', 5, f), 'abc')
+                assert.is.equal(utils.textAtWidth('abcd', 6, f), 'abcd')
+            end)
+        end)
+
+        describe('one character == 0.7 unit length', function()
+            local f = {
+                getWidth = function (_, text)
+                    return text:len() * 0.7
+                end
+            }
+
+            it('empty text', function()
+                assert.is.equal(utils.textAtWidth('', 0, f), '')
+                assert.is.equal(utils.textAtWidth('', 1, f), '')
+                assert.is.equal(utils.textAtWidth('', 2, f), '')
+                assert.is.equal(utils.textAtWidth('', 10, f), '')
+            end)
+
+            it('1-character text', function()
+                assert.is.equal(utils.textAtWidth('a', 0, f), '')
+                assert.is.equal(utils.textAtWidth('a', 1, f), 'a')
+                assert.is.equal(utils.textAtWidth('a', 2, f), 'a')
+                assert.is.equal(utils.textAtWidth('a', 10, f), 'a')
+            end)
+
+            it('general', function()
+                assert.is.equal(utils.textAtWidth('abcdefg', 0, f), '')
+                assert.is.equal(utils.textAtWidth('abcdefg', 1, f), 'a')
+                assert.is.equal(utils.textAtWidth('abcdefg', 2, f), 'ab')
+                assert.is.equal(utils.textAtWidth('abcdefg', 3, f), 'abcd')
+                assert.is.equal(utils.textAtWidth('abcdefg', 4, f), 'abcde')
+                assert.is.equal(utils.textAtWidth('abcdefg', 5, f), 'abcdefg')
+                assert.is.equal(utils.textAtWidth('abcdefg', 6, f), 'abcdefg')
+            end)
+        end)
+    end)
+end)
+
